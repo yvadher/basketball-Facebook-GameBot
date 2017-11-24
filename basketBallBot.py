b3c7848accaa5a748ffa1398d461f8389fbbbeaa
Yagnik Vadher <yagniktechmail@gmail.com>
2017-11-24 04:31:28 -0800

    Added darg and drop lower level

--- basketBallBot.py
+++ basketBallBot.py
@@ -1,6 +1,15 @@
-Settings.MoveMouseDelay = 0.1
+import time
+
+
+Settings.MoveMouseDelay = 0.3
+
 wait("1511519641842.png")
 
 for i in range(40):
-        dragDrop(Pattern("1511519708984.png").targetOffset(-2,18), Pattern("1511519720025.png").targetOffset(-1,-54))
+        if (exists("__CLICK-TO-CAPTURE__")):
+            
+        drag(Pattern("1511519708984.png").targetOffset(-1,18))
+        dropAt(Pattern("1511519720025.png").targetOffset(-1,-59))
+        ##dragDrop(Pattern("1511519708984.png").targetOffset(-1,18),Pattern("1511519720025.png").targetOffset(-1,-59) )
+        
         
