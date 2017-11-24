d299abd877487863f353508b0c266dfb6dbe234a
Yagnik Vadher <yagniktechmail@gmail.com>
2017-11-24 03:11:03 -0800

    Initial commit

---
+++ basketBallBot.py
@@ -0,0 +1,6 @@
+Settings.MoveMouseDelay = 0.1
+wait("1511519641842.png")
+
+for i in range(40):
+        dragDrop(Pattern("1511519708984.png").targetOffset(-2,18), Pattern("1511519720025.png").targetOffset(-1,-54))
+        
